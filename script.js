8e35b73b61767cc32031c2ac3a5482bcf5c69835
Miguel92 <joelmiguelvalente@gmail.com>
2022-08-15 17:24:47 -0300

    Actualización
    Se verifica que solo ingrese las letras desde la "A" hasta la "Z", incluyendo los espacios!

--- script.js
+++ script.js
@@ -27,45 +27,59 @@ var mapObjClave = { ai: "a", enter: "e", imes: "i", ober: "o", ufat: "u" };
 var regexV = /a|e|i|o|u/img;
 var regexC = /ai|enter|imes|ober|ufat/img;
 
+// Continuamos
+var continuar = true;
+
 function generador(accion) {
 	var nuevo = [];
    var letra = mensaje.value;
-	// Forzamos a minusculas
-	letra = letra.toLowerCase()
-	// Buscamos y reemplazamos
-	letra = letra.replace((accion ? regexV : regexC), matched => (accion ? mapObjVocal[matched] : mapObjClave[matched]));
+   // https://elcodigoascii.com.ar/
+   var espacio = 32, empieza = 97, termina = 122;
+   // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+   letra.split('').filter(word => {
+   	if((empieza > word.charCodeAt(0) || termina < word.charCodeAt(0)) && word.charCodeAt(0) != espacio) {
+   		alert('No permitido: ' + word)
+   		continuar = false;
+   	}
+  	});
+  	if(continuar) {
+		// Forzamos a minusculas
+		letra = letra.toLowerCase()
+		// Buscamos y reemplazamos
+		letra = letra.replace((accion ? regexV : regexC), matched => (accion ? mapObjVocal[matched] : mapObjClave[matched]));
 
-	resultado.innerHTML = letra;
-	mensaje.value = '';
-	if(accion) {
-		boton_encriptar.classList.add('wait')
-		boton_encriptar.innerHTML += ' <span id="load"><i></i> espere...</span>'
-	} else {
-		boton_desencriptar.classList.add('wait')
-		boton_desencriptar.innerHTML += ' <span id="load"><i></i> espere...</span>'
-	}
-	setTimeout(() => {
+		resultado.innerHTML = letra;
+		mensaje.value = '';
 		if(accion) {
-			boton_encriptar.classList.remove('wait')
-			boton_encriptar.removeChild(document.getElementById("load"))
+			boton_encriptar.classList.add('wait')
+			boton_encriptar.innerHTML += ' <span id="load"><i></i> espere...</span>'
 		} else {
-			boton_desencriptar.classList.remove('wait')
-			boton_desencriptar.removeChild(document.getElementById("load"))
+			boton_desencriptar.classList.add('wait')
+			boton_desencriptar.innerHTML += ' <span id="load"><i></i> espere...</span>'
 		}
-		if(screen.width <= 560) {
-			/*
-			 * https://developer.mozilla.org/es/docs/Web/API/Element/clientHeight
-			*/
-			window.scroll({
-			  	top: document.querySelector(".area-input").offsetHeight + 20,
-			  	behavior: 'smooth'
-			})
-		}
-		mostrar.removeAttribute('style')
-	}, 2000) // 2s
+		setTimeout(() => {
+			if(accion) {
+				boton_encriptar.classList.remove('wait')
+				boton_encriptar.removeChild(document.getElementById("load"))
+			} else {
+				boton_desencriptar.classList.remove('wait')
+				boton_desencriptar.removeChild(document.getElementById("load"))
+			}
+			if(screen.width <= 560) {
+				/*
+				 * https://developer.mozilla.org/es/docs/Web/API/Element/clientHeight
+				*/
+				window.scroll({
+				  	top: document.querySelector(".area-input").offsetHeight + 20,
+				  	behavior: 'smooth'
+				})
+			}
+			mostrar.removeAttribute('style')
+		}, 2000) // 2s
+	}
 }
 
-function copiar(){
+async function copiar(){
    resultado.removeAttribute("disabled")
    resultado.select();
    // Por alguna razón no me funciona
